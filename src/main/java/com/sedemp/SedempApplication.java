c19da55961b8fdebdc39005bee1ca7bda6bec122
Elias Calado <eliascalado@hotmail.com>
2021-06-07 10:11:06 -0300

    Projeto craido

---
+++ src/main/java/com/sedemp/SedempApplication.java
@@ -0,0 +1,13 @@
+package com.sedemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SedempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SedempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sedemp/SedempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SedempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
